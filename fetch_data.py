ec7af5b1e22c9139ad9d40b13155e68464ef336a
howsmyscripts-code <howsmyscripts@gmail.com>
2026-05-17 03:26:42 +0200

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -7,13 +7,13 @@ STOCKS = [
     "INVE-B.ST", "ATCO-B.ST", "SWED-A.ST", "SAAB-B.ST", "ERIC-B.ST",
     "VOLV-B.ST", "EQT.ST", "HM-B.ST", "SEB-A.ST", "TEL2-B.ST",
     # Europa
-    "ASML", "SAP", "NVO", "LVMUY", "SHEL", "SIEGY", "NSRGY", "EADSY", "AZN",
+    "ASML", "SAP", "NVO", "LVMUY", "SHEL", "SIEGY", "NSRGY", "EADSY", "AZN", "RELX", "BAESY",
     # USA
     "NVDA", "AAPL", "MSFT", "AMZN", "GOOGL", "META", "TSLA", "JPM", "BRK-B", "LLY",
     # Asien - Kina
     "BABA", "TCEHY", "PDD", "BYDDF", "JD",
     # Asien - Taiwan
-    "TSM", "CAMT", "LOGI",
+    "TSM", "CAMT",
     # Asien - Japan
     "TM", "SONY", "NTDOY", "FANUY", "MUFG",
     # Asien - Sydkorea
