df4123f7e699d9d6ecea80db3aaeb6a11105fa2a
howsmyscripts-code <howsmyscripts@gmail.com>
2026-05-27 10:16:09 +0200

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -60,7 +60,7 @@ STOCKS = [
     "TM", "SONY", "NTDOY", "FANUY", "MUFG",
     "005930.KS", "005380.KS", "000660.KS",
     "CL=F", "GC=F", "SI=F", "BTC-USD", "ETH-USD",
-    "CSPX.L", "EQQQ.DE", "XACT.ST", "XACTHDIV.ST", "SMH", "DFNS.L", "VWRL.L", "IEMG", "IQQH.DE", "GOLD.L",
+    "CSPX.L", "EQQQ.DE", "XACT-OMXS30.ST", "XACTHDIV.ST", "SMH", "DFNS.L", "VWRL.L", "IEMG", "IQQH.DE", "IGLN.L",
 ]
 
 # Stocks that don't report earnings
